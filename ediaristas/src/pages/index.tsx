30dd4dd951da08b1373b3a3cb520fd6ad3d6326f
Paulo <paulo_sanches2012@hotmail.com>
2021-06-15 22:22:13 -0300

    initial web project

---
+++ ediaristas/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import SafeEnvironment from "ui/components/FeedBack/SafeEnvironment";
+
+export default function Home() {
+  return (
+    <div>
+      <SafeEnvironment />
+    </div>
+  )
+}
---
+++ ediaristas/src/ui/components/FeedBack/SafeEnvironment/index.tsx
@@ -0,0 +1,9 @@
+import { SafeEnverionmentContainer } from './styles.style'
+
+const SafeEnvironment = () => {
+  return (
+      <SafeEnverionmentContainer>SafeEnvironment</SafeEnverionmentContainer>
+  );
+}
+
+export default SafeEnvironment;
