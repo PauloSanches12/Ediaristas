acc3c58f46336dd79094b241d8433990047f23ff
Paulo <paulo_sanches2012@hotmail.com>
2021-10-23 16:42:04 -0300

    adicionando projeto mobile

---
+++ e-diaristas-backend/src/main/java/com/paulo/ediaristasbackend/EDiaristasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.paulo.ediaristasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDiaristasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDiaristasBackendApplication.class, args);
+	}
+
+}
